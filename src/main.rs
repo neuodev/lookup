fb75ce5180310fc6f98444f1855136528e0f721f
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-07 10:48:34 +0200

    Get args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Search files line by line using regular expression
+#[derive(Parser, Debug)]
+#[clap(author = "Ahmed Ibrahim", version, about, long_about = None)]
+struct Args {
+    /// File to search
+    #[clap(short, long, value_parser)]
+    file: String,
+    /// Regular experssion
+    #[clap(short, long, value_parser)]
+    regex: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
